a0fdf57c56d7227549ff9596579dd1fd209c8b87
ShivankKMishra <96675359+ShivankKMishra@users.noreply.github.com>
2025-10-31 09:16:40 +0000

    ghA3

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,8 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+
+        // On .NET 8, this is valid and recommended in non‑Shell apps
         MainPage = new MainPage();
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -8,10 +8,12 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
+                // Add fonts here if needed
             });
 
         return builder.Build();
